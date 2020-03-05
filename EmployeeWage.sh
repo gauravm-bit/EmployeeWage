3cce831aed50b80f0bab8070f8d80fd256f6b2d2
gauravm-bit <gauravwrk01@gmail.com>
2020-03-05 12:16:59 +0530

    ADDED:daily Employee Wage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,9 +1,15 @@
 #!/bin/bash -x
 
+wagePerHour=20
+
 echo "Welcome to Employee wage problem"
 if [[ $((RANDOM%2)) == 1 ]]
 then
+	empHour=8
 	echo "Employee is present"
 else
+	empHour=0
 	echo "Employee is absent"
 fi
+dailyWage=$(( $wagePerHour*$empHour ))
+echo "Dailywage is  :" $dailyWage
