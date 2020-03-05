7d868666545109ac91bbd0889e255ba7d1258858
gauravm-bit <gauravwrk01@gmail.com>
2020-03-05 13:19:48 +0530

    ADDED:Parttime_wage_employee

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,9 +1,20 @@
 #!/bin/bash -x
 
+wagePerHour=20
+randomCheck=$(( RANDOM%3 ))
+
 echo "Welcome to Employee wage problem"
-if [[ $((RANDOM%2)) == 1 ]]
+if [[ $randomCheck == 1 ]]
 then
-	echo "Employee is present"
+	empHour=8
+	echo "Employee is present and fulltime"
+elif [[ $randomCheck == 2 ]]
+then
+	empHour=4
+	echo "Employee is present and parttime"
 else
+	empHour=0
 	echo "Employee is absent"
 fi
+dailyWage=$(( $wagePerHour * $empHour ))
+echo "Dailywage is  :" $dailyWage
