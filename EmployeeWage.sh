81c614295a22763ab1da52cfecf553b1cac82450
gauravm-bit <gauravwrk01@gmail.com>
2020-03-05 11:40:43 +0530

    ADDED: Employee attendance

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,2 +1,9 @@
-echo "Welcome to Employee wage problem"
+#!/bin/bash -x
 
+echo "Welcome to Employee wage problem"
+if [[ $((RANDOM%2)) == 1 ]]
+then
+	echo "Employee is present"
+else
+	echo "Employee is absent"
+fi
