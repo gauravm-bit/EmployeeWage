92ca449427108475374b86bdf4fbca0bbc44aef6
gauravm-bit <gauravwrk01@gmail.com>
2020-03-05 15:20:29 +0530

    ADDED:monthly_wage_calculation

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,20 +1,23 @@
 #!/bin/bash -x
 wagePerHour=20
-randomCheck=$(( RANDOM%3 ))
 
 echo "Welcome to Employee wage problem"
 
-case $randomCheck in
-	0)echo "Employee is absent"
-	empHour=0
-	;;
-	1)echo "Employee is present and fulltime"
-	empHour=8
-	;;
-	2)echo "Employee is present and parttime"
-	empHour=4
-	;;
-esac
-
-dailyWage=$(( $wagePerHour*$empHour ))
-echo "Dailywage is  :" $dailyWage
+for (( day=1; day<=20; day++ ))
+do
+	randomCheck=$(( RANDOM%3 ))
+	case $randomCheck in
+		0)echo "Employee is absent"
+		empHour=0
+		;;
+		1)echo "Employee is present and fulltime"
+		empHour=8
+		;;
+		2)echo "Employee is present and parttime"
+		empHour=4
+		;;
+	esac
+	salary=$(( $wagePerHour*$empHour ))
+ 	totalSalary=$(( $totalSalary +$salary ))
+done
+echo "Monthly wage is "$totalSalary
